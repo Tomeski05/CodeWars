31d682666566acbac14e572988a0a718a5f5603b
Tomeski05 <tomeski@protonmail.com>
2022-02-11 16:04:31 +0100

    8kyu

---
+++ 8kyu/8kyu/8kyu/Reversed Strings.cs
@@ -0,0 +1,23 @@
+﻿
+
+
+//Complete the solution so that it reverses the string passed into it.
+
+//'world'  =>  'dlrow'
+//'word'   =>  'drow'
+
+
+using System;
+
+public static class Kata
+{
+    public static string Solution(string str)
+    {
+        string rev = "";
+        for (int i = str.Length; i > 0; i--)
+        {
+            rev += str[i - 1];
+        }
+        return rev;
+    }
+}
