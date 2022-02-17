b3dd20cef070eec74dddd322873f776997b48aa1
Tomeski05 <tomeski@protonmail.com>
2022-02-18 00:55:20 +0100

    Solutions for 6kyu

---
+++ 6kyu/6kyu/Count characters in your string.cs
@@ -0,0 +1,31 @@
+﻿
+
+
+//The main idea is to count all the occurring characters in a string. If you have a string like aba, then the result should be {'a': 2, 'b': 1}.
+
+//What if the string is empty ? Then the result should be empty object literal, {}.
+
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Kata
+{
+    public static Dictionary<char, int> Count(string str)
+    {
+        var result = new Dictionary<char, int>();
+
+        foreach (var uniqueChar in str.Distinct())
+        {
+            var countChar = 0;
+            foreach (var ch in str)
+                if (uniqueChar == ch)
+                    countChar++;
+            result.Add(uniqueChar, countChar);
+        }
+
+        return result;
+    }
+}
