f2a1e886b500c4389e5b3d21f83622ba0a03aa5a
Tomeski05 <tomeski@protonmail.com>
2022-02-18 02:17:50 +0100

    Solutions for 4kyu

---
+++ 4kyu/4kyu/Next smaller number with the same digits.cs
@@ -0,0 +1,43 @@
+﻿
+
+
+//Write a function that takes a positive integer and returns the next smaller positive integer containing the same digits.
+
+//For example:
+
+//nextSmaller(21) == 12
+//nextSmaller(531) == 513
+//nextSmaller(2071) == 2017
+//Return - 1(for Haskell: return Nothing, for Rust: return None), when there is no smaller number that contains the same digits. Also return -1 when the next smaller number with the same digits would require the leading digit to be zero.
+
+//nextSmaller(9) == -1
+//nextSmaller(111) == -1
+//nextSmaller(135) == -1
+//nextSmaller(1027) == -1 // 0721 is out since we don't write numbers with leading zeros
+//some tests will include very large numbers.
+//test data only employs positive integers.
+
+
+
+using System;
+using System.Linq;
+
+public class Kata
+{
+    public static long NextSmaller(long n)
+    {
+        var chars = n.ToString();
+        for (var i = chars.Length - 1; i > 0; i--)
+        {
+            if (chars[i] < chars[i - 1])
+            {
+                var right = chars.Skip(i).ToArray();
+                var next = right.Where(a => a < chars[i - 1]).Max();
+                right[Array.IndexOf(right, next)] = chars[i - 1];
+                var answer = chars.Substring(0, i - 1) + next + string.Concat(right.OrderByDescending(a => a));
+                return answer[0] == '0' ? -1 : long.Parse(answer);
+            }
+        }
+        return -1;
+    }
+}
---
+++ 4kyu/4kyu/Snail.cs
@@ -0,0 +1,77 @@
+﻿
+
+
+
+//Snail Sort
+//Given an n x n array, return the array elements arranged from outermost elements to the middle element, traveling clockwise.
+
+//array = [[1,2,3],
+//         [4,5,6],
+//         [7,8,9]]
+//snail(array) #=> [1,2,3,6,9,8,7,4,5]
+//For better understanding, please follow the numbers of the next array consecutively:
+
+//array = [[1, 2, 3],
+//         [8,9,4],
+//         [7,6,5]]
+//snail(array) #=> [1,2,3,4,5,6,7,8,9]
+
+
+
+using System;
+using System.Collections.Generic;
+
+public class SnailSolution
+{
+    public static int[] Snail(int[][] array)
+    {
+        int left = 0;
+        int right = array[0].Length - 1;
+        int top = 0;
+        int bottom = array.Length - 1;
+
+        List<int> result = new List<int>();
+        while (true)
+        {
+            if (left > right)
+            {
+                return result.ToArray();
+            }
+
+            if (left == right)
+            {
+                for (int i = top; i <= bottom; i++)
+                {
+                    result.Add(array[i][right]);
+                }
+
+                return result.ToArray();
+            }
+
+            for (int i = left; i < right; i++)
+            {
+                result.Add(array[top][i]);
+            }
+
+            for (int i = top; i < bottom; i++)
+            {
+                result.Add(array[i][right]);
+            }
+
+            for (int i = right; i > left; i--)
+            {
+                result.Add(array[bottom][i]);
+            }
+
+            for (int i = bottom; i > top; i--)
+            {
+                result.Add(array[i][left]);
+            }
+
+            left++;
+            right--;
+            top++;
+            bottom--;
+        }
+    }
+}
