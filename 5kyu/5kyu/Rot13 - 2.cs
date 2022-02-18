a6beaebf650545605c1546df7367a33b8d52053a
Tomeski05 <tomeski@protonmail.com>
2022-02-18 01:57:50 +0100

    Solutions for 5kyu

---
+++ 5kyu/5kyu/Rot13 - 2.cs
@@ -0,0 +1,27 @@
+﻿
+
+
+//ROT13 is a simple letter substitution cipher that replaces a letter with the letter 13 letters after it in the alphabet.
+//ROT13 is an example of the Caesar cipher.
+
+//Create a function that takes a string and returns the string ciphered with Rot13.
+//If there are numbers or special characters included in the string, they should be returned as they are.
+//Only letters from the latin/english alphabet should be shifted, like in the original Rot13 "implementation".
+
+
+using System.Linq;
+
+public class Kata
+{
+    public static string Rot13(string message)
+    {
+        return string.Concat(message.Select(s =>
+        {
+            if (s >= 'a' && s <= 'z')
+                return (char)((s - 'a' + 13) % 26 + 'a');
+            if (s >= 'A' && s <= 'Z')
+                return (char)((s - 'A' + 13) % 26 + 'A');
+            return s;
+        }));
+    }
+}
---
+++ 5kyu/5kyu/Simple Pig Latin.cs
@@ -0,0 +1,34 @@
+﻿
+
+
+
+//Move the first letter of each word to the end of it, then add "ay" to the end of the word. Leave punctuation marks untouched.
+
+//Examples
+//Kata.PigIt("Pig latin is cool"); // igPay atinlay siay oolcay
+//Kata.PigIt("Hello world !");     // elloHay orldway !
+
+
+
+using System;
+
+public class Kata
+{
+    public static string PigIt(string str)
+    {
+        string[] words = str.Split(' ');
+        string result = "";
+        foreach (string word in words)
+        {
+            if (word.Length > 1)
+            {
+                result += word.Substring(1) + word[0] + "ay ";
+            }
+            else
+            {
+                result += word + " ";
+            }
+        }
+        return result.Trim();
+    }
+}
